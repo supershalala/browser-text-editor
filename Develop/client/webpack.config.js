8cd83a4376e6e8020094c34244aea4ef461e0061
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-06-29 13:34:18 +1000

    edited manifest atributes

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -34,9 +34,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Simple Text Editor',
+        short_name: 'Text-Edot',
+        description: 'Just another text editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
