01b5ced9eecb9c5abcb1b518ee198a27bd10bf72
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-06-29 13:23:15 +1000

    install.js file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. -done
+// TODO: Add CSS loaders and babel to webpack.- done 
 
 module.exports = () => {
   return {
