2b03f780af2c071289eb972aa14072f6a97b65ae
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-06-29 13:45:48 +1000

    database.js logic

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.put({content});
+    await tx.done;
+    console.log('Content added to the database');
+  } catch (error) {
+    console.error('Error in putDb:', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const content = await store.getAll();
+    await tx.done;
+    console.log('Content retrieved from the database');
+    return content;
+  } catch (error) {
+    console.error('Error in getDb:', error);
+  }
+};
 
 initdb();
